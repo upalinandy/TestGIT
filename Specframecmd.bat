a8c339602d0328ef32ffe9cec1351a203121cf3b
upalinandy <nandyupali@gmail.com>
2016-11-24 15:07:07 +0530

    correct the path to default.srprofile

--- Specframecmd.bat
+++ Specframecmd.bat
@@ -1,1 +1,1 @@
-"packages\SpecRun.Runner.1.5.2\tools\SpecRun.exe" run Default.srprofile /baseFolder:"SpecFrame\bin\Release" /log:specrun.log
+"packages\SpecRun.Runner.1.5.2\tools\SpecRun.exe" run Default.srprofile /baseFolder:"SpecFramework\bin\Release" /log:specrun.log
